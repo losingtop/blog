b07df134d229f029d8d7f2df077827579fa9055b
losing <59799716+losingtop@users.noreply.github.com>
2023-05-14 19:59:42 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,12 +27,22 @@ if (postId) {
 
         const template = document.querySelector('template#post')
         const postElement = template.content.cloneNode(true)
+        
+        if (post?.tags) {
+            const tag = postElement.querySelector('a.tag')
+            
+            for (tag of post.tags) {
+                const tagElement = tag.content.cloneNode(true)
+                tagElement.setAttribute('href', `/#/tag/${tag}`)
+                tagElement.innerText = tag
+            }
+        } else
+            postElement.querySelector('.tags').remove()
 
         post?.image ? postElement.querySelector('.image').setAttribute('src', post.image) : postElement.querySelector('.image').remove()
         postElement.querySelector('.title').innerText = post.title
         post?.description ? postElement.querySelector('.description').innerText = post.description : postElement.querySelector('.description').remove()
         postElement.querySelector('.details').innerText = `By ${post.author.displayName}`
-        post?.tags ? postElement.querySelector('.tags').innerText = `${post.tags}` : postElement.querySelector('.tags').remove()
         postElement.querySelector('.dates').innerText = `Created: ${new Date(post.createdDate).toLocaleDateString()} ${new Date(post.createdDate).toLocaleTimeString()}\n Changed: ${new Date(post.updatedDate).toLocaleDateString()} ${new Date(post.updatedDate).toLocaleTimeString()}`
         postElement.querySelector('.content').innerHTML = marked.parse(post.content)
 
