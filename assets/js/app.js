3197026f67e1b54a21101903eb76b31156fe90d1
losing <59799716+losingtop@users.noreply.github.com>
2023-05-19 14:35:37 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -254,7 +254,7 @@ async function loadContent(urlPath) {
             wrapper.append(postElement)
             postsWrapper.append(wrapper)
             
-            wrapper.animate([{ transform: "translateX(120vw)" }, { transform: "translateX(0)" }], { duration: 300, iterations: 1, delay: 100 * i })
+            wrapper.animate([{ transform: "translateX(120vw)" }, { transform: "translateX(0)" }], { duration: 300, iterations: 1, delay: (300 * i) })
         }
     }  else if (urlPath.startsWith('#/author')) {
         const authorId = urlPath.replace('#/author/', '')
